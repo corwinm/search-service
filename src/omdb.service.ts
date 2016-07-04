71aa9add06c5a28743ee7d4e9ddb029f16242ea0
Corwin Marsh <corwinm@slalom.com>
2016-07-03 18:23:15 -0700

    fix references

--- src/omdb.service.ts
+++ src/omdb.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, Inject } from "@angular/core";
 import { Http, RequestOptionsArgs, URLSearchParams } from '@angular/http';
 
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { OmdbMovie } from "./models";
 
